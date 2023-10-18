32f8fb87e8b1a4742f376de3edf7b34f1e90c6c1
yuqiao <1207427994@qq.com>
2023-10-18 14:52:54 +0800

    add arr & obj fn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const money = require("./src/moneyFormat")
 const browser = require("./src/browser")
 const strFormat = require("./src/strFormat")
+const obj = require("./src/obj")
+const arr = require("./src/arr")
 
 module.exports = {
   ...money,
   ...browser,
   ...strFormat,
+  ...obj,
+  ...arr,
 }
